9eb0b4cae861c73a1028bb735bbd55a4b9c472ec
Rajat <rajat@eleven01.io>
2019-09-06 14:51:28 +0530

    http lib

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const CronJob = require('cron').CronJob;
-const httpCall = require('./httpCall');
+const httpCall = require('./lib/httpCall');
 const config = require('./config.json');
 
-const app = express()
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const RPC_PREFIX = 'http://';
 const RPC_IP = config.RPC_SERVER;
@@ -15,15 +15,15 @@ var count = 0;
 var ip = RPC_IP[count];
 var RPC_URL = RPC_PREFIX + ip;
 
-app.all('/', async (request, response)=> {
+app.all('/', async (request, response) => {
 
 	const args = request.body;
 	const res = await httpCall.call('POST', RPC_URL, args);
 	response.json(res);
 });
 
-const job = new CronJob('0 */1 * * * *', async ()=> {
-	console.log('Started Cron1 At:', new Date());
+const job = new CronJob('0 */1 * * * *', async () => {
+	console.log('Started Cron At:', new Date());
 	RPC_URL = RPC_PREFIX + ip;
 	var res = await httpCall.call('POST', RPC_URL, { jsonrpc: '2.0', id: 2, method: 'net_listening', params: [] });
 	console.log("net_listening res: ", RPC_URL, res);
@@ -33,8 +33,7 @@ const job = new CronJob('0 */1 * * * *', async ()=> {
 		ip = RPC_IP[count];
 	}
 	if(!res.result) {
-		count ++;
-		ip = RPC_IP[count];
+		ip = RPC_IP[count++];
 	}
 	if(!res.result && RPC_IP.length == count) {
 		count = 0;
